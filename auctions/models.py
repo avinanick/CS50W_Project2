28e6f2f2c7832f04fbd24a7fde644a370ff92aa2
Nick Avina <avinanick@gmail.com>
2020-09-10 21:38:09 -0700

    Added the listing field to the comment model.

--- auctions/models.py
+++ auctions/models.py
@@ -5,14 +5,15 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Comment(models.Model):
-    poster = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=1920)
-
 class AuctionListing(models.Model):
     name = models.CharField(max_length=128)
     image_url = models.CharField(max_length=128)
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1920)
 
 class AuctionBid(models.Model):
     bidder = models.ForeignKey(User, on_delete=models.CASCADE)
