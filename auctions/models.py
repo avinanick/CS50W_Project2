a50e8047ea00c1cd5939e671e709cab518f5149e
Nick Avina <avinanick@gmail.com>
2020-09-10 22:17:54 -0700

    Added description and starting bid fields to the auction listing model.

--- auctions/models.py
+++ auctions/models.py
@@ -6,7 +6,9 @@ class User(AbstractUser):
     pass
 
 class AuctionListing(models.Model):
-    name = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=1920)
+    starting_bid = models.FloatField()
     image_url = models.CharField(max_length=128)
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
 
