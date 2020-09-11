cbf9da24335a590665ff1662c0aad12ad9c77b0e
Nick Avina <avinanick@gmail.com>
2020-09-10 19:25:10 -0700

    Added dummy classes for the models that will be used for listings, bids, and comments.

--- auctions/models.py
+++ auctions/models.py
@@ -2,5 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class AuctionListing(models.Model):
+    pass
+
+class AuctionBid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
 class User(AbstractUser):
     pass
