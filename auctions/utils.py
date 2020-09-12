219fbe9b2e14a2d1bacb1ad3a1a251d53d6b0efa
Nick Avina <avinanick@gmail.com>
2020-09-11 21:53:46 -0700

    Fixed an issue where I was trying to get the wrong field name in calculate price.

--- auctions/utils.py
+++ auctions/utils.py
@@ -1,7 +1,7 @@
 from .models import User, AuctionListing, AuctionBid, Comment
 
 def calculate_price(auction):
-    highest_bid = auction.bids.order_by("-amount").first()
+    highest_bid = auction.listing_bids.order_by("-amount").first()
     price = auction.starting_bid
     if highest_bid:
         price = highest_bid
