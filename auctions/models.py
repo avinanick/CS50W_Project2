a2f21e84c90e90ffa35388b3fbad1ac3a8389e10
Nick Avina <avinanick@gmail.com>
2020-09-12 19:04:35 -0700

    Added a category section in the auction listing model.

--- auctions/models.py
+++ auctions/models.py
@@ -8,6 +8,9 @@ class User(AbstractUser):
 class Category(models.Model):
     type=models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.type
+
 class AuctionListing(models.Model):
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=1920)
@@ -17,6 +20,7 @@ class AuctionListing(models.Model):
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     watchers = models.ManyToManyField(User, related_name="watchlist")
     is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, related_name="listings", default=Category.objects.get(type="Home"))
 
 class Comment(models.Model):
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
