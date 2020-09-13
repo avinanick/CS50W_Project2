793253d4898c0a59adc319c6b0ccaac5e0619b45
Nick Avina <avinanick@gmail.com>
2020-09-12 19:03:23 -0700

    Registered models to be editable for the superuser.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AuctionListing, AuctionBid, Comment, Category
 
 # Register your models here.
+admin.site.register(AuctionListing)
+admin.site.register(AuctionBid)
+admin.site.register(Comment)
+admin.site.register(Category)
