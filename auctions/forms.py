5b14ed8efa19703aa649cff04d9de5d58156fd25
Nick Avina <avinanick@gmail.com>
2020-09-12 19:04:01 -0700

    Added a category section in the listing form for creating a new auction.

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Category
 
 class BidForm(forms.Form):
     bid = forms.DecimalField(min_value=0, decimal_places=2, label="Enter Bid")
@@ -11,3 +12,4 @@ class ListingForm(forms.Form):
     description = forms.CharField(max_length=1920, widget=forms.Textarea, initial="Description")
     starting_bid = forms.DecimalField(min_value=0, decimal_places=2)
     image_url = forms.CharField(max_length=128)
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
