ed888c7fae9fe4c14bc852a603343ed92e669ea7
Nick Avina <avinanick@gmail.com>
2020-09-12 17:10:06 -0700

    Added a form for comments.

--- auctions/forms.py
+++ auctions/forms.py
@@ -3,6 +3,9 @@ from django import forms
 class BidForm(forms.Form):
     bid = forms.DecimalField(min_value=0, decimal_places=2, label="Enter Bid")
 
+class CommentForm(forms.Form):
+    comment = forms.CharField(max_length=1920, initial="Comment", widget=forms.Textarea)
+
 class ListingForm(forms.Form):
     title = forms.CharField(max_length=128, label="Listing Title")
     description = forms.CharField(max_length=1920, widget=forms.Textarea, initial="Description")
